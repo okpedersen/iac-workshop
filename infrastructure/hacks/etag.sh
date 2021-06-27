b6659763f5fec8a163caf8fe07d1d2ca81f9e49a
Ole Kristian Pedersen <okpedersen@gmail.com>
2021-06-27 12:29:59 +0200

    Handle etag extraction failure

--- infrastructure/hacks/etag.sh
+++ infrastructure/hacks/etag.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eou pipefail
 URL=$1
-etag=$(curl -sIL "$URL" | awk 'BEGIN {FS=": "}/^etag/{print $2}' | tr -d '"' | tr -d '\r')
+etag=$(curl -sfIL "$URL" | awk 'BEGIN {FS=": "}/^etag/{print $2}' | tr -d '"' | tr -d '\r' || (echo "Extraction of etag failed for $URL" >&2 && exit 1))
 echo "{ \"etag\": \"$etag\" }"

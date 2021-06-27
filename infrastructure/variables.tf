43e5c5508973f45756378aedbeaf01f91d7d3d13
Ole Kristian Pedersen <okpedersen@gmail.com>
2021-06-27 18:49:07 +0200

    Remove some default vars from variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -6,12 +6,10 @@ variable "location" {
 
 variable "backend_image" {
   type        = string
-  default     = "ghcr.io/evenh/iac-workshop-backend:latest"
   description = "The Docker image to run for the backend"
 }
 
 variable "frontend_zip" {
   type        = string
-  default     = "https://github.com/bekk/iac-workshop/releases/latest/download/iac-workshop-frontend.zip"
   description = "URL to ZIP containing the compiled frontend"
 }
